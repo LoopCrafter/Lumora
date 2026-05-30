12a86daff2b7e65bb5c434504975e32b0200b4f9
Hamed <ostovari.eng@gmail.com>
2026-05-30 18:24:00 +0200

    Configured Neo + drizzle kit

---
+++ db/schema.ts
@@ -0,0 +1,12 @@
+import { integer, pgTable, timestamp, varchar } from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }).notNull(),
+  email: varchar({ length: 255 }).notNull().unique(),
+  imageUrl: varchar().notNull(),
+  credit: integer().default(3),
+  createdAt: timestamp({ mode: "date" }).defaultNow().notNull(),
+  lastVisitTime: timestamp({ mode: "date" }).defaultNow().notNull(),
+  userId: varchar({ length: 255 }).notNull(),
+});
